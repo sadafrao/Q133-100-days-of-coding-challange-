0e4d39c9c8befddbf6dd2440ccee891b2a239f0b
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-15 18:10:07 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// Q133
+// define a javascripit object
+var person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+// convert object into j sons string 
+var json = JSON.stringify(person);
+console.log(json);
---
+++ index.ts
@@ -0,0 +1,10 @@
+// Q133
+// define a javascripit object
+let person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+// convert object into j sons string 
+let json = JSON.stringify(person);
+console.log(json);
